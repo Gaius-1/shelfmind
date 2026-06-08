c9fb413a15dfab9c1fa892aa144b3cc53ebe02ca
Freddie <agbofrederick230@gmail.com>
2026-06-08 07:18:15 +0000

    chore: add Vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import tailwindcss from '@tailwindcss/vite'
+// import { cloudflare } from '@cloudflare/vite-plugin'
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    // cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+    babel({ presets: [reactCompilerPreset()] }),
+  ],
+  build: {
+    rollupOptions: {
+      external: ['@better-auth/kysely-adapter', 'kysely'],
+    },
+  },
+})
+
+export default config
