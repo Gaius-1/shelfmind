0337f50c529fdab0e4a1289eb95490861955ceca
Freddie <agbofrederick230@gmail.com>
2026-06-13 20:11:35 +0000

    Refine UI component: nav-secondary.tsx

--- src/components/nav-secondary.tsx
+++ src/components/nav-secondary.tsx
@@ -24,7 +24,7 @@ export function NavSecondary({
         <SidebarMenu>
           {items.map((item) => (
             <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton asChild size="sm">
+              <SidebarMenuButton asChild className="text-[13px] font-medium h-9">
                 <a href={item.url}>
                   {item.icon}
                   <span>{item.title}</span>
