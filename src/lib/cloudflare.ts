72faeff0a5d1122765c0c3f826157d4e2fb13f6a
Freddie <agbofrederick230@gmail.com>
2026-06-14 12:55:36 +0000

    fix(cloudflare): automatically strip UTF-8 BOM from string bindings

--- src/lib/cloudflare.ts
+++ src/lib/cloudflare.ts
@@ -1,5 +1,9 @@
 export function getBinding(name: string): any {
-  return (globalThis as any)[name] || (process.env as any)[name]
+  const val = (globalThis as any)[name] || (process.env as any)[name]
+  if (typeof val === 'string' && val.startsWith('\ufeff')) {
+    return val.slice(1)
+  }
+  return val
 }
 
 
